4bd523eea4d43f8f8b0452dfbff2db3cd821d0ad
Micah-Pirkisi <micahpirikisi@gmail.com>
2025-10-16 10:49:32 +0200

    Created hash(key) function to produce hash codes from keys

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 class HashMap {
-    constructor(loadFactor = 0.75, capacity = 16) {
-        this.loadFactor = loadFactor; 
-        this.capacity = capacity; 
+  constructor(loadFactor = 0.75, capacity = 16) {
+    this.loadFactor = loadFactor;
+    this.capacity = capacity;
+    this.size = 0;
+    this.buckets = new Array(this.capacity).fill(null).map(() => []);
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
     }
+
+    return hashCode;
+  }
 }
